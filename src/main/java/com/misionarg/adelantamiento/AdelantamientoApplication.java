aeb92324d8b6ef0dbe08b7695278ecfd53c5b573
Braian Marquez <braianmarq95@gmail.com>
2022-12-24 09:30:59 -0300

    primer commit

---
+++ src/main/java/com/misionarg/adelantamiento/AdelantamientoApplication.java
@@ -0,0 +1,13 @@
+package com.misionarg.adelantamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdelantamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdelantamientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misionarg/adelantamiento/AdelantamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misionarg.adelantamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdelantamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
